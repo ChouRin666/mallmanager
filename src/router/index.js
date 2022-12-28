2608606c412a52a7c1f7ebc91860bbc0cc1ebe36
Cavons <magic_agate@163.com>
2022-12-28 21:49:13 +0800

    “新建登录组件并配置路由”

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import HelloWorld from '@/components/HelloWorld';
+// 传统写法，此处弃用
+// import Login from '../components/login/login'
+// webpack 封装好路径，通过 @ 寻找 src 文件夹下的文件。  @ -> src
+import Login from '@/components/login/login'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/'
-      // name: 'HelloWorld',
-      // component: HelloWorld,
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
   ],
 });
